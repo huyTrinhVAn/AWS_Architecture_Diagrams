b51872298751ed77d3815d28f091cf6fb4b99f95
huyTrinhVAn <128719193+huyTrinhVAn@users.noreply.github.com>
2025-11-06 16:47:00 +1100

    feat(app): adding another diagram

--- aws-diagrams/src/data/diagrams.ts
+++ aws-diagrams/src/data/diagrams.ts
@@ -1,6 +1,7 @@
 // Import tất cả ảnh
 import dataLake from '../assets/diagrams/data_lake.png';
 import DDOS from '../assets/diagrams/DDOS.png';
+import dashboardImage from '../assets/diagrams/dashboard_diagram.png'
 export interface Diagram {
     id: number;
     title: string;
@@ -20,5 +21,11 @@ export const diagrams: Diagram[] = [
         title: "Cross-Account S3 Event Handling with Queues and Lambda (Anti-DDoS Design)",
         description: "This setup isolates uploads from multiple consumer accounts. Each S3 bucket event (s3:ObjectCreated:*) is sent to a dedicated SQS queue in a central security account, preventing direct Lambda invocation spikes.Queues buffer traffic and route messages to per-consumer Lambdas that share common scanning code. Failed messages go to DLQs for reliability.The design reduces DDoS risk, smooths event bursts, and keeps scanning consistent and secure across accounts",
         imageUrl: DDOS  // Dùng imported variable
+    },
+    {
+        id: 3,
+        title: "Serverless Event-Driven Health Monitoring Pipeline (Multi-Account)",
+        description: "CloudWatch Alarms and other event-based services publish to EventBridge in consumer accounts, which route events to a central distribution account. SQS buffers the traffic; a Lambda (configured via SSM) transforms the payload, triggers SNS alerts, and persists records in MongoDB. The Health Dashboard reads MongoDB to visualize real-time status and trends.",
+        imageUrl: dashboardImage  // Dùng imported variable
     }
 ];

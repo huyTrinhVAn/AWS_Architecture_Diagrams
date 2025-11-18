0fbc3e3cc4b080fac85fca121dec29308101dcd7
huyTrinhVAn <128719193+huyTrinhVAn@users.noreply.github.com>
2025-11-18 22:59:01 +1100

    feat(app): adding analystics to the webpage

--- aws-diagrams/src/App.tsx
+++ aws-diagrams/src/App.tsx
@@ -1,11 +1,12 @@
 
 import DiagramGallery from './components/DiagramGallery';
 import './App.css';
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <div className="App">
       <DiagramGallery />
+      <Analytics />
     </div>
   );
 }
